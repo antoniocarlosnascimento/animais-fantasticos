6dee816019109b84d777c51f8de2b160fa201e73
Toni Nascimento <antonio.carlosnascimento@outlook.com>
2020-05-15 05:30:42 -0300

    Mudou posicao initAnimaScroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
@@ -10,6 +9,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 // const options = { behavior: "smooth", block: "start" };
 // const scrollSuave = new ScrollSuave(
@@ -19,7 +19,6 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 // scrollSuave.init();
 new ScrollSuave('[data-menu="suave"] a[href^="#"]').init();
 
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -30,6 +29,7 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
 
 // import $ from "jquery";
 // import _ from "lodash";
